075a2e9c91fde001fde13b7d3724ed639a4e9f80
Caoxiaobai1 <965074528qq@gmail.com>
2025-09-07 21:23:32 +0800

    update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757247910531";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1757251042976";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
