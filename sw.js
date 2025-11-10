6b6fbb22e896ed14867e847064222fb6a9425fe9
caoyifei <cyf02293472@taobao.com>
2025-11-10 16:21:52 +0800

    Site updated: 2025-11-10 16:21:38

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1762759998211";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1762762883286";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
